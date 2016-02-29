33c08983e840c65ba5a61be7c0570976eed5c6e1
Николай Тур < >
2016-02-29 18:12:40 +0700

    New Repository

---
+++ Data.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+
+double adjacentValuesUpward(double DATA)
+// Конвертирование соседних величин из меньшей в большую, не касается конвертиирования битов в байты;
+{
+    DATA = DATA/1024;
+    return DATA;
+}
+double adjacentValuesToDecrease(double DATA)
+// Конвертирование соседних величиин из большей в меньшую, не касается конвертирования байтов в биты;
+{
+    DATA = DATA*1024;
+    return DATA;
+}
+double valuesBetweenOneUpward(double DATA)
+{
+    DATA = DATA/(pow(1024, 2));
+    return DATA;
+}
+double valuesBetweenOneToDecrease(double DATA)
+{
+    DATA = DATA*(pow(1024, 2));
+    return DATA;
+}
+double valuesBetweenOTwoUpward(double DATA)
+{
+    DATA = DATA/(pow(1024, 3));
+    return DATA;
+}
+double valuesBetweenTwoToDecrease(double DATA)
+{
+    DATA = DATA*(pow(1024, 3));
+    return DATA;
+}
+double valuesBetweenThreeUpward(double DATA)
+{
+    DATA = DATA/(pow(1024, 4));
+    return DATA;
+}
+double valuesBetweenThreeToDecrease(double DATA)
+{
+    DATA = DATA*(pow(1024, 4));
+    return DATA;
+}
+double valuesBetweenFourUpward(double DATA)
+{
+    DATA = DATA/(pow(1024, 5));
+    return DATA;
+}
+double valuesBetweenFourToDecrease(double DATA)
+{
+    DATA = DATA*(pow(1024, 5));
+    return DATA;
+}
+double valuesBetweenFiveUpward(double DATA)
+{
+    DATA = DATA/(pow(1024, 6));
+    return DATA;
+}
+double valuesBetweenFiveToDecrease(double DATA)
+{
+    DATA = DATA*(pow(1024, 6));
+    return DATA;
+}
+double valuesBetweenSixUpward(double DATA)
+{
+    DATA = DATA/(pow(1024, 7));
+    return DATA;
+}
+double valuesBetweenSixToDecrease(double DATA)
+{
+    DATA = DATA*(pow(1024, 7));
+    return DATA;
+}
+double bitsTobytes(double DATA)
+{
+    DATA = DATA/8;
+    return DATA;
+}
+double bytesTobits(double DATA)
+{
+    DATA = DATA*8;
+    return DATA;
+}
---
+++ main.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<locale.h>
+#include<math.h>
+
+int DATA;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    int output_value;
+
+    printf("Я теперь понимаю русский язык!))\n\n");
+
+    system("pause");
+    exit(EXIT_SUCCESS);
+}
