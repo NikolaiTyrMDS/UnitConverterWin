34f7a868df4d52188dc9e3e70abdfce331733a33
Николай Тур < >
2016-03-02 15:10:38 +0700

    The beginning of Radiation converter started at the moment

---
+++ Radiation.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+// Конвертирование соседних в сторону увеличения;
+double adjacentValuesUpward(double RESISTANCE)
+{
+    RESISTANCE = RESISTANCE/1000;
+    return RESISTANCE;
+}
+// ----------------------------------------------------- //
+// Конвертирование соседних в сторону уменьшения;
+double adjacentValuesToDecrease(double RESISTANCE)
+{
+    RESISTANCE = RESISTANCE*1000;
+    return RESISTANCE;
+}
+// ----------------------------------------------------- //
