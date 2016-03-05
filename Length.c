eb4640af456270c35de48955876c5c03664526c8
Arsenyi Lisenko <Lisenko.A95@yandex.ru>
2016-03-05 21:25:21 +0600

    There were some difficulties, but it is finished

--- Length.c
+++ Length.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-                // Метрические единицы измерения  мм, см, метр, км //
+                // - Метрические единицы измерения - мм, см, метр, км ( дм, ???) -//
 
 /////////////////////////////Миллиметры/////////////////////////////////////////////
 double millimetresTOcentimetres(double LENGTH) // Перевод из миллиметров в сантиметры
@@ -86,4 +86,116 @@ double kilometresTOmetres(double LENGTH) // Перевод из километров в метры
 ///////////////////////////////////////////////////////////////////////////////////
 
 
-                           //Английская система мер фут, ярд, миля(морская), миля(земная), Дюйм //
+                //-Английская система мер - фут, ярд, миля(морская), миля(земная), Дюйм -//
+
+//--------------------------------------ФУТЫ--------------------------------------------------//
+double FootTOYard(double LENGTH) // Перевод из футов  в ярды
+{
+    LENGTH = LENGTH / 3;
+     return LENGTH;
+}
+
+
+double FootTONauticalMile(double LENGTH) // Перевод из футов в морскую милю.
+{
+    LENGTH = LENGTH / 6076.1154855643 ft;// нужно уточннить
+     return LENGTH;
+}
+
+
+double FootTOStatuteMile(double LENGTH) // Перевод из футов в земную  милю.
+{
+    LENGTH = LENGTH / 5280.0000010236 ft (survey) ft;// нужно уточннить
+     return LENGTH;
+}
+
+
+double FootTOInch(double LENGTH) // Перевод из футов в дюйм.
+{
+    LENGTH = LENGTH * 12;
+     return LENGTH;
+}
+//--------------------------------------------------------------------------------------------//
+
+//--------------------------------------ЯРДЫ--------------------------------------------------//
+double YardTOFoot(double LENGTH) // Перевод из ярдов  в футы
+{
+    LENGTH = LENGTH * 3;
+     return LENGTH;
+}
+
+double YardTONauticalMile(double LENGTH) // Перевод из ярдов  в морские милю
+{
+    LENGTH = LENGTH / 2025.3718285214 yd // надо уточнить
+     return LENGTH;
+}
+
+
+double YardTOStatuteMile(double LENGTH) // Перевод из ярдов  в земную  милю
+{
+    LENGTH = LENGTH /1760.........;//уточнить
+     return LENGTH;
+}
+
+double YardTOInch(double LENGTH) // Перевод из ярдов  в дюйм
+{
+    LENGTH = LENGTH * 36;
+     return LENGTH;
+}
+
+//--------------------------------------Миля Морская--------------------------------------------------//
+double NauticalMileTOFoot(double LENGTH) // Перевод из морской милли в футы
+{
+    LENGTH = LENGTH * 6076.1154855643 ft;// уточнить
+     return LENGTH;
+}
+
+double NauticalMileTOYard(double LENGTH) // Перевод из морской милли в ярды
+{
+    LENGTH = LENGTH * 2025.3718285214 yd;// уточнить
+    return LENGTH;
+}
+
+double NauticalMileTOStatuteMile(double LENGTH) // Перевод из морской милли в земную милю
+{
+    LENGTH = LENGTH ??????????????????;// уточнить
+     return LENGTH;
+}
+
+double NauticalMileTOInch(double LENGTH) // Перевод из морской милли в  дюйм
+{
+    LENGTH = LENGTH / 1.3714902807775E-5 miles;
+     return LENGTH;
+}
+
+//--------------------------------------Миля земная--------------------------------------------------//
+
+
+//---------------------------------------------------------------------------------------------------//
+
+
+//--------------------------------------Дюйм---------------------------------------------------------//
+double InchTOFoot(double LENGTH) // Перевод из дюйма  в футы
+{
+    LENGTH = LENGTH / 12;
+     return LENGTH;
+}
+
+double InchTOYard(double LENGTH) // Перевод из дюйма  в ярд
+{
+    LENGTH = LENGTH / 36;
+     return LENGTH;
+}
+
+double InchTONauticalMile(double LENGTH) // Перевод из дюйма  в Морскую милю
+{
+    LENGTH = LENGTH / 72913.385826772 in;// уточню
+     return LENGTH;
+}
+
+double InchTOStatuteMile(double LENGTH) // Перевод из дюйма  в Земную  милю
+{
+    LENGTH = LENGTH / !????????????????
+     return LENGTH;
+}
+//---------------------------------------------------------------------------------------------------//
