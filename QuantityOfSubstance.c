a35fdb9c50041adfec4911fd76fd7a3b43a03c6b
ProkopenkoRoman <long399@mail.ru>
2016-03-17 19:19:58 +0600

    Update QuantityOfSubstance.c
    В модуль добавлены:
    1) <мегамоли>;
    2) <микромоли>.

--- QuantityOfSubstance.c
+++ QuantityOfSubstance.c
@@ -74,3 +74,111 @@ double PoundMole2kMole(double QOS) {
 	QOS = (QOS / 1000) / 0.0022046226;
 	return QOS;
 }
+
+// <Моли> ---> <Микромоли> //
+double Mole2MkMole(double QOS) {
+	QOS = QOS * 1000000;
+	return QOS;
+}
+
+// <Микромоли> ---> <Моли> //
+double MkMole2Mole(double QOS) {
+	QOS = QOS / 1000000;
+	return QOS;
+}
+
+// <Киломоли> ---> <Микромоли> //
+double kMole2MkMole(double QOS) {
+	QOS = QOS * 1000000000;
+	return QOS;
+}
+
+// <Микромоли> ---> <Киломоли> //
+double MkMole2kMole(double QOS) {
+	QOS = QOS / 1000000000;
+	return QOS;
+}
+
+// <Миллимоли> ---> <Микромоли> //
+double mMole2MkMole(double QOS) {
+	QOS = QOS * 1000;
+	return QOS;
+}
+
+// <Микромоли> ---> <Миллимоли> //
+double MkMole2mMole(double QOS) {
+	QOS = QOS / 1000;
+	return QOS;
+}
+
+// <Фунт-моли> ---> <Микромоли> //
+double PoundMole2MkMole(double QOS) {
+	QOS = (QOS * 1000000) / 0.0022046226;
+	return QOS;
+}
+
+// <Микромоли> ---> <Фунт-моли> //
+double MkMole2PoundMole(double QOS) {
+	QOS = QOS * 1000000 * 0.0022046226;
+	return QOS;
+}
+
+// <Моли> ---> <Мегамоли> //
+double Mole2MgMole(double QOS) {
+	QOS = QOS / 1000000;
+	return QOS;
+}
+
+// <Мегамоли> ---> <Моли> //
+double MgMole2Mole(double QOS) {
+	QOS = QOS * 1000000;
+	return QOS;
+}
+
+// <Киломоли> ---> <Мегамоли> //
+double kMole2MgMole(double QOS) {
+	QOS = QOS / 1000;
+	return QOS;
+}
+
+// <Мегамоли> ---> <Киломоли> //
+double MgMole2kMole(double QOS) {
+	QOS = QOS * 1000;
+	return QOS;
+}
+
+// <Миллимоли> ---> <Мегамоли> //
+double mMole2MgMole(double QOS) {
+	QOS = QOS / 1000000000;
+	return QOS;
+}
+
+// <Мегамоли> ---> <Миллимоли> //
+double MgMole2mMole(double QOS) {
+	QOS = QOS * 1000000000;
+	return QOS;
+}
+
+// <Микромоли> ---> <Мегамоли> //
+double MkMole2MgMole(double QOS) {
+	QOS = QOS / 1000000000000;
+	return QOS;
+}
+
+// <Мегамоли> ---> <Микромоли> //
+double MgMole2MkMole(double QOS) {
+	QOS = QOS * 1000000000000;
+	return QOS;
+}
+
+// <Фунт-моли> ---> <Мегамоли> //
+double PoundMole2MgMole(double QOS) {
+	QOS = (QOS * 1000000) / 0.0022046226;
+	return QOS;
+}
+
+// <Мегамоли> ---> <Фунт-моли> //
+double MgMole2PoundMole(double QOS) {
+	QOS = (QOS / 1000000) * 0.0022046226;
+	return QOS;
+}
