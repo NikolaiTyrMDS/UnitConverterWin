a06ac8a64891d4a050734d7cfdda95d30efbe5f7
Николай Тур < >
2016-03-23 15:27:03 +0700

    SWITCH line

--- main.c
+++ main.c
@@ -2,14 +2,35 @@
 #include<stdlib.h>
 #include<locale.h>
 #include<math.h>
+#include"data.h"
 
 int main()
 {
     setlocale(LC_ALL, "Russian");
 
-    printf("Я теперь понимаю русский язык!))\n\n");
+    double result, value;
+    char ch;
 
-    printf("Сий конвертер сможет конвертировать для Вас любые (или почти любые) величины в считанные мгновения!);");
+    printf("ertrtuytfdhcg: \n");
+    scanf("%lf", &value);
+    printf("ifdzxcvcbngjhjg?;\n");
+    ch = getch();
+    printf("\n");
+
+    switch(ch) {
+        case '1':
+            result = adjacentValuesUpward(value);
+            printf("Result is %lf", result);
+            break;
+        case '2':
+            result = adjacentValuesToDecrease(value);
+            printf("Result is %lf", result);
+            break;
+        default:
+            printf("ERROR");
+            break;
+    }
+    printf("\n\n");
 
     system("pause");
     exit(EXIT_SUCCESS);
