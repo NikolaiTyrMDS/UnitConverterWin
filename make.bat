75c1b7adc8222b13545bf2e6428d0a1f31639be7
NikolaiTyrOverlordMASSADAMDS <NEWTYPEDEITY@live.ru>
2016-04-06 12:43:01 +0600

    ReorganizationOfDirectory
    ReorganizationOfDirectory;

--- make.bat
+++ make.bat
@@ -1,3 +1,3 @@
-gcc.exe -o E:\UNIT_Converter\MAIN.EXE E:\UNIT_Converter\data.h E:\UNIT_Converter\Data.c E:\UNIT_Converter\main.c
+gcc.exe -o E:\UNIT_Converter\CurrentVersion\MAIN.EXE E:\UNIT_Converter\data.h E:\UNIT_Converter\Data.c E:\UNIT_Converter\main.c
 pause
 exit
