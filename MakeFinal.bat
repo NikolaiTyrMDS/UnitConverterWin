fb5a3ff50a1629e335c2603d261b8371ec3f1f89
NikolaiTyrMASSADA <NEWTYPEDEITY@live.ru>
2016-04-08 14:00:54 +0600

    Added *.bat files for building versions of Converter

---
+++ MakeFinal.bat
@@ -0,0 +1,3 @@
+gcc.exe -Wall -o D:\Documents\GitHub\UNIT_Converter\Converter.exe D:\Documents\GitHub\UNIT_Converter\Project\Modules\Data.c D:\Documents\GitHub\UNIT_Converter\Project\Modules\Main.c D:\Documents\GitHub\UNIT_Converter\Project\Modules\selectionswitchData.c
+pause
+exit
