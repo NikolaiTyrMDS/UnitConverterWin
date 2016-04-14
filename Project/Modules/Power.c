13e164a79e320486a9ccea46548f39e0b3da9557
ProkopenkoRoman <long399@mail.ru>
2016-04-14 17:25:55 +0600

    Update Power.c
    В модуль добавлены:
    1) Милливаты;
    2) Микроваты.

--- Project/Modules/Power.c
+++ Project/Modules/Power.c
@@ -253,3 +253,183 @@ double HorsePowEng2HorsePowMetr(double POWER) {
 	POWER = POWER * 1.014;
 	return POWER;
 }
+
+// <Ваты> ---> <Милливаты> //
+double Vat2MlVat(double POWER) {
+	POWER = POWER * 1000;
+	return POWER;
+}
+
+// <Милливаты> ---> <Ваты> //
+double MlVat2Vat(double POWER) {
+	POWER = POWER / 1000;
+	return POWER;
+}
+
+// <Киловаты> ---> <Милливаты> //
+double kVat2MlVat(double POWER) {
+	POWER = POWER * 1000000;
+	return POWER;
+}
+
+// <Милливаты> ---> <Киловаты> //
+double MlVat2kVat(double POWER) {
+	POWER = POWER / 1000000;
+	return POWER;
+}
+
+// <Мегаваты> ---> <Милливаты> //
+double mVat2MlVat(double POWER) {
+	POWER = POWER * 1000000000;
+	return POWER;
+}
+
+// <Милливаты> ---> <Мегаваты> //
+double MlVat2mVat(double POWER) {
+	POWER = POWER / 1000000000;
+	return POWER;
+}
+
+// <Милливаты> ---> <Килограмм-сила-метр в секунду> //
+double MlVat2kgSilaMvS(double POWER) {
+	POWER = POWER / 9.81 / 1000;
+	return POWER;
+}
+
+// <Килограмм-сила-метр в секунду> ---> <Милливаты> //
+double kgSilaMvS2MlVat(double POWER) {
+	POWER = (POWER * 9.81) / 1000;
+	return POWER;
+}
+
+// <Милливаты> ---> <Эрг в секунду> //
+double MlVat2ErgS(double POWER) {
+	POWER = POWER  * 10000;
+	return POWER;
+}
+
+// <Эрг в секунду> ---> <Милливаты> //
+double ErgS2MlVat(double POWER) {
+	POWER = POWER / 10000;
+	return POWER;
+}
+
+// <Милливаты> ---> <Лошадиные силы(метрические)> //
+double MlVat2HorsePowMetr(double POWER) {
+	POWER = POWER  / 735.5 / 1000;
+	return POWER;
+}
+
+// <Лошадиные силы(метрические)> ---> <Милливаты> //
+double HorsePowMetr2MlVat(double POWER) {
+	POWER = (POWER * 735.5) / 1000;
+	return POWER;
+}
+
+// <Милливаты> ---> <Лошадиные силы(английские)> //
+double MlVat2HorsePowEng(double POWER) {
+	POWER = POWER / 745.7 / 1000;
+	return POWER;
+}
+
+// <Лошадиные силы(английские)> ---> <Милливаты> //
+double HorsePowEng2MlVat(double POWER) {
+	POWER = (POWER * 745.7) / 1000;
+	return POWER;
+}
+
+// <Милливаты> ---> <Микроваты> //
+double MlVat2MkVat(double POWER) {
+	POWER = POWER * 1000;
+	return POWER;
+}
+
+// <Микроваты> ---> <Милливаты> //
+double MkVat2MlVat(double POWER) {
+	POWER = POWER / 1000;
+	return POWER;
+}
+
+// <Ваты> ---> <Микроваты> //
+double Vat2MkVat(double POWER) {
+	POWER = POWER * 1000000;
+	return POWER;
+}
+
+// <Микроваты> ---> <Ваты> //
+double MkVat2Vat(double POWER) {
+	POWER = POWER / 1000000;
+	return POWER;
+}
+
+// <Киловаты> ---> <Микроваты> //
+double kVat2MkVat(double POWER) {
+	POWER = POWER * 1000000000;
+	return POWER;
+}
+
+// <Микроваты> ---> <Киловаты> //
+double MkVat2kVat(double POWER) {
+	POWER = POWER / 1000000000;
+	return POWER;
+}
+
+// <Мегаваты> ---> <Микроваты> //
+double mVat2MkVat(double POWER) {
+	POWER = POWER * 1000000000000;
+	return POWER;
+}
+
+// <Микроваты> ---> <Мегаваты> //
+double MkVat2mVat(double POWER) {
+	POWER = POWER / 1000000000000;
+	return POWER;
+}
+
+// <Микроваты> ---> <Килограмм-сила-метр в секунду> //
+double MkVat2kgSilaMvS(double POWER) {
+	POWER = POWER / 9.81 / 1000000;
+	return POWER;
+}
+
+// <Килограмм-сила-метр в секунду> ---> <Микроваты> //
+double kgSilaMvS2MkVat(double POWER) {
+	POWER = (POWER * 9.81) / 1000000;
+	return POWER;
+}
+
+// <Микроваты> ---> <Эрг в секунду> //
+double MkVat2ErgS(double POWER) {
+	POWER = POWER  * 10;
+	return POWER;
+}
+
+// <Эрг в секунду> ---> <Микроваты> //
+double ErgS2MkVat(double POWER) {
+	POWER = POWER / 10;
+	return POWER;
+}
+
+// <Микроваты> ---> <Лошадиные силы(метрические)> //
+double MkVat2HorsePowMetr(double POWER) {
+	POWER = POWER  / 735.5 / 1000000;
+	return POWER;
+}
+
+// <Лошадиные силы(метрические)> ---> <Микроваты> //
+double HorsePowMetr2MkVat(double POWER) {
+	POWER = (POWER * 735.5) / 1000000;
+	return POWER;
+}
+
+// <Микроваты> ---> <Лошадиные силы(английские)> //
+double MkVat2HorsePowEng(double POWER) {
+	POWER = POWER / 745.7 / 1000000;
+	return POWER;
+}
+
+// <Лошадиные силы(английские)> ---> <Микроваты> //
+double HorsePowEng2MkVat(double POWER) {
+	POWER = (POWER * 745.7) / 1000000;
+	return POWER;
+}
