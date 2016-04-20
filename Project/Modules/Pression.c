88233a95a450d94cdebb7a06202a97eb3cae3847
PopovMihail <popov-mm@ya.ru>
2016-04-20 15:37:47 +0700

    Модуль конвертирования давления

---
+++ Project/Modules/Pression.c
@@ -0,0 +1,668 @@
+#include <stdio.h>
+
+//bar
+double bar_to_mbar(double PRESS) {
+	PRESS = PRESS * 1000;
+	return PRESS;
+}
+
+double bar_to_paskal(double PRESS) {
+	PRESS = PRESS * 100000;
+	return PRESS;
+}
+
+double bar_to_kpaskal(double PRESS) {
+	PRESS = PRESS * 100;
+	return PRESS;
+}
+
+double bar_to_Mpaskal(double PRESS) {
+	PRESS = PRESS * 0.1;
+	return PRESS;
+}
+
+double bar_to_gcm(double PRESS) {
+	PRESS = PRESS * 1020;
+	return PRESS;
+}
+
+double bar_to_kgcm(double PRESS) {
+	PRESS = PRESS * 1.02;
+	return PRESS;
+}
+
+double bar_to_toncm(double PRESS) {
+	PRESS = PRESS * 0.00102;
+	return PRESS;
+}
+
+double bar_to_kgm(double PRESS) {
+	PRESS = PRESS * 10200;
+	return PRESS;
+}
+
+double bar_to_tonm(double PRESS) {
+	PRESS = PRESS * 10.2;
+	return PRESS;
+}
+
+double bar_to_newtoncm(double PRESS) {
+	PRESS = PRESS * 10;
+	return PRESS;
+}
+
+double bar_to_newtonm(double PRESS) {
+	PRESS = PRESS * 100000;
+	return PRESS;
+}
+//mbar
+double mbar_to_bar(double PRESS) {
+	PRESS = PRESS / 1000;
+	return PRESS;
+}
+
+double mbar_to_paskal(double PRESS) {
+	PRESS = PRESS * 100;
+	return PRESS;
+}
+
+double mbar_to_kpaskal(double PRESS) {
+	PRESS = PRESS * 0.1;
+	return PRESS;
+}
+
+double mbar_to_gpaskal(double PRESS) {
+	
+	return PRESS;
+}
+
+double mbar_to_Mpaskal(double PRESS) {
+	PRESS = PRESS * 0.0001;
+	return PRESS;
+}
+
+double mbar_to_gcm(double PRESS) {
+	PRESS = PRESS * 1.02;
+	return PRESS;
+}
+
+double mbar_to_kgcm(double PRESS) {
+	PRESS = PRESS * 0.00102;
+	return PRESS;
+}
+
+double mbar_to_toncm(double PRESS) {
+	PRESS = PRESS * 0.00000102;
+	return PRESS;
+}
+
+double mbar_to_kgm(double PRESS) {
+	PRESS = PRESS * 10.2;
+	return PRESS;
+}
+
+double mbar_to_tonm(double PRESS) {
+	PRESS = PRESS * 0.0102;
+	return PRESS;
+}
+
+double mbar_to_newtoncm(double PRESS) {
+	PRESS = PRESS * 0.01;
+	return PRESS;
+}
+
+double mbar_to_newtonm(double PRESS) {
+	PRESS = PRESS * 100;
+	return PRESS;
+}
+//paskal
+double paskal_to_bar(double PRESS) {
+	PRESS = PRESS * 0.00001;
+	return PRESS;
+}
+
+double paskal_to_mbar(double PRESS) {
+	PRESS = PRESS / 100;
+	return PRESS;
+}
+
+double paskal_to_kpaskal(double PRESS) {
+	PRESS = PRESS / 1000;
+	return PRESS;
+}
+
+double paskal_to_Mpaskal(double PRESS) {
+	PRESS = PRESS / 1000000;
+	return PRESS;
+}
+
+double paskal_to_gcm(double PRESS) {
+	PRESS = PRESS * 0.0102;
+	return PRESS;
+}
+
+double paskal_to_kgcm(double PRESS) {
+	PRESS = PRESS * 0.0000102;
+	return PRESS;
+}
+
+double paskal_to_toncm(double PRESS) {
+	PRESS = PRESS * 0.0000000102;
+	return PRESS;
+}
+
+double paskal_to_kgm(double PRESS) {
+	PRESS = PRESS * 0.102;
+	return PRESS;
+}
+
+double paskal_to_tonm(double PRESS) {
+	PRESS = PRESS * 0.000102;
+	return PRESS;
+}
+
+double paskal_to_newtoncm(double PRESS) {
+	PRESS = PRESS * 0.0001;
+	return PRESS;
+}
+
+double paskal_to_newtonm(double PRESS) {
+	
+	return PRESS;
+}
+//kpaskal
+double kpaskal_to_bar(double PRESS) {
+	PRESS = PRESS * 0.01;
+	return PRESS;
+}
+
+double kpaskal_to_mbar(double PRESS) {
+	PRESS = PRESS * 10;
+	return PRESS;
+}
+
+double kpaskal_to_paskal(double PRESS) {
+	PRESS = PRESS * 1000;
+	return PRESS;
+}
+
+double kpaskal_to_Mpaskal(double PRESS) {
+	PRESS = PRESS / 1000;
+	return PRESS;
+}
+
+double kpaskal_to_gcm(double PRESS) {
+	PRESS = PRESS * 10.2;
+	return PRESS;
+}
+
+double kpaskal_to_kgcm(double PRESS) {
+	PRESS = PRESS * 0.0102;
+	return PRESS;
+}
+
+double kpaskal_to_toncm(double PRESS) {
+	PRESS = PRESS * 0.0000102;
+	return PRESS;
+}
+
+double kpaskal_to_kgm(double PRESS) {
+	PRESS = PRESS * 102;
+	return PRESS;
+}
+
+double kpaskal_to_tonm(double PRESS) {
+	PRESS = PRESS * 0.102;
+	return PRESS;
+}
+
+double kpaskal_to_newtoncm(double PRESS) {
+	PRESS = PRESS * 0.1;
+	return PRESS;
+}
+
+double kpaskal_to_newtonm(double PRESS) {
+	PRESS = PRESS * 1000;
+	return PRESS;
+}
+
+//Mpaskal
+double Mpaskal_to_bar(double PRESS) {
+	PRESS = PRESS * 10;
+	return PRESS;
+}
+
+double Mpaskal_to_mbar(double PRESS) {
+	PRESS = PRESS * 10000;
+	return PRESS;
+}
+
+double Mpaskal_to_paskal(double PRESS) {
+	PRESS = PRESS * 1000000;
+	return PRESS;
+}
+
+double Mpaskal_to_kpaskal(double PRESS) {
+	PRESS = PRESS * 1000;
+	return PRESS;
+}
+
+double Mpaskal_to_gcm(double PRESS) {
+	PRESS = PRESS * 10200;
+	return PRESS;
+}
+
+double Mpaskal_to_kgcm(double PRESS) {
+	PRESS = PRESS * 10.2;
+	return PRESS;
+}
+
+double Mpaskal_to_toncm(double PRESS) {
+	PRESS = PRESS * 0.0102;
+	return PRESS;
+}
+
+double Mpaskal_to_kgm(double PRESS) {
+	PRESS = PRESS * 102000;
+	return PRESS;
+}
+
+double Mpaskal_to_tonm(double PRESS) {
+	PRESS = PRESS * 102;
+	return PRESS;
+}
+
+double Mpaskal_to_newtoncm(double PRESS) {
+	PRESS = PRESS * 100;
+	return PRESS;
+}
+
+double Mpaskal_to_newtonm(double PRESS) {
+	PRESS = PRESS * 1000000;
+	return PRESS;
+}
+//gcm
+double gcm_to_bar(double PRESS) {
+	PRESS = PRESS * 0.0009807;
+	return PRESS;
+}
+
+double gcm_to_mbar(double PRESS) {
+	PRESS = PRESS * 0.9807;
+	return PRESS;
+}
+
+double gcm_to_paskal(double PRESS) {
+	PRESS = PRESS * 98.07;
+	return PRESS;
+}
+
+double gcm_to_kpaskal(double PRESS) {
+	PRESS = PRESS * 0.09807;
+	return PRESS;
+}
+
+double gcm_to_Mpaskal(double PRESS) {
+	PRESS = PRESS * 0.00009807;
+	return PRESS;
+}
+
+double gcm_to_kgcm(double PRESS) {
+	PRESS = PRESS / 1000;
+	return PRESS;
+}
+
+double gcm_to_toncm(double PRESS) {
+	PRESS = PRESS / 1000000;
+	return PRESS;
+}
+
+double gcm_to_kgm(double PRESS) {
+	PRESS = PRESS * 10;
+	return PRESS;
+}
+
+double gcm_to_tonm(double PRESS) {
+	PRESS = PRESS / 100;
+	return PRESS;
+}
+
+double gcm_to_newtoncm(double PRESS) {
+	PRESS = PRESS * 0.009807;
+	return PRESS;
+}
+
+double gcm_to_newtonm(double PRESS) {
+	PRESS = PRESS * 98.07;
+	return PRESS;
+}
+//kgcm
+double kgcm_to_bar(double PRESS) {
+	PRESS = PRESS * 0.9807;
+	return PRESS;
+}
+
+double kgcm_to_mbar(double PRESS) {
+	PRESS = PRESS * 980.7;
+	return PRESS;
+}
+
+double kgcm_to_paskal(double PRESS) {
+	PRESS = PRESS * 98070;
+	return PRESS;
+}
+
+double kgcm_to_kpaskal(double PRESS) {
+	PRESS = PRESS * 98.07;
+	return PRESS;
+}
+
+double kgcm_to_Mpaskal(double PRESS) {
+	PRESS = PRESS * 0.09807;
+	return PRESS;
+}
+
+double kgcm_to_gcm(double PRESS) {
+	PRESS = PRESS * 1000;
+	return PRESS;
+}
+
+double kgcm_to_toncm(double PRESS) {
+	PRESS = PRESS / 1000;
+	return PRESS;
+}
+
+double kgcm_to_kgm(double PRESS) {
+	PRESS = PRESS * 10000;
+	return PRESS;
+}
+
+double kgcm_to_tonm(double PRESS) {
+	PRESS = PRESS * 10;
+	return PRESS;
+}
+
+double kgcm_to_newtoncm(double PRESS) {
+	PRESS = PRESS * 9.807;
+	return PRESS;
+}
+
+double kgcm_to_newtonm(double PRESS) {
+	PRESS = PRESS * 98070;
+	return PRESS;
+}
+//toncm
+double toncm_to_bar(double PRESS) {
+	PRESS = PRESS * 980.7;
+	return PRESS;
+}
+
+double toncm_to_mbar(double PRESS) {
+	PRESS = PRESS * 980700;
+	return PRESS;
+}
+
+double toncm_to_paskal(double PRESS) {
+	PRESS = PRESS * 98070000;
+	return PRESS;
+}
+
+double toncm_to_kpaskal(double PRESS) {
+	PRESS = PRESS * 98070;
+	return PRESS;
+}
+
+double toncm_to_Mpaskal(double PRESS) {
+	PRESS = PRESS * 98.07;
+	return PRESS;
+}
+
+double toncm_to_gcm(double PRESS) {
+	PRESS = PRESS * 1000000;
+	return PRESS;
+}
+
+double toncm_to_kgcm(double PRESS) {
+	PRESS = PRESS * 1000;
+	return PRESS;
+}
+
+double toncm_to_kgm(double PRESS) {
+	PRESS = PRESS * 10000000;
+	return PRESS;
+}
+
+double toncm_to_tonm(double PRESS) {
+	PRESS = PRESS * 10000;
+	return PRESS;
+}
+
+double toncm_to_newtoncm(double PRESS) {
+	PRESS = PRESS * 9807;
+	return PRESS;
+}
+
+double toncm_to_newtonm(double PRESS) {
+	PRESS = PRESS * 98070000;
+	return PRESS;
+}
+//kgm
+double kgm_to_bar(double PRESS) {
+	PRESS = PRESS * 0.00009807;
+	return PRESS;
+}
+
+double kgm_to_mbar(double PRESS) {
+	PRESS = PRESS * 0.09807;
+	return PRESS;
+}
+
+double kgm_to_paskal(double PRESS) {
+	PRESS = PRESS * 9.807;
+	return PRESS;
+}
+
+double kgm_to_kpaskal(double PRESS) {
+	PRESS = PRESS * 0.009807;
+	return PRESS;
+}
+
+double kgm_to_Mpaskal(double PRESS) {
+	PRESS = PRESS * 0.000009807;
+	return PRESS;
+}
+
+double kgm_to_gcm(double PRESS) {
+	PRESS = PRESS / 10;
+	return PRESS;
+}
+
+double kgm_to_kgcm(double PRESS) {
+	PRESS = PRESS / 10000;
+	return PRESS;
+}
+
+double kgm_to_toncm(double PRESS) {
+	PRESS = PRESS / 10000000;
+	return PRESS;
+}
+
+double kgm_to_tonm(double PRESS) {
+	PRESS = PRESS / 1000;
+	return PRESS;
+}
+
+double kgm_to_newtoncm(double PRESS) {
+	PRESS = PRESS * 0.0009807;
+	return PRESS;
+}
+
+double kgm_to_newtonm(double PRESS) {
+	PRESS = PRESS * 9.807;
+	return PRESS;
+}
+//tonm
+double tonm_to_bar(double PRESS) {
+	PRESS = PRESS * 0.09807;
+	return PRESS;
+}
+
+double tonm_to_mbar(double PRESS) {
+	PRESS = PRESS * 98.07;
+	return PRESS;
+}
+
+double tonm_to_paskal(double PRESS) {
+	PRESS = PRESS * 9807;
+	return PRESS;
+}
+
+double tonm_to_kpaskal(double PRESS) {
+	PRESS = PRESS * 9.807;
+	return PRESS;
+}
+
+double tonm_to_Mpaskal(double PRESS) {
+	PRESS = PRESS * 0.009807;
+	return PRESS;
+}
+
+double tonm_to_gcm(double PRESS) {
+	PRESS = PRESS * 100;
+	return PRESS;
+}
+
+double tonm_to_kgcm(double PRESS) {
+	PRESS = PRESS / 10;
+	return PRESS;
+}
+
+double tonm_to_toncm(double PRESS) {
+	PRESS = PRESS / 10000;
+	return PRESS;
+}
+
+double tonm_to_kgm(double PRESS) {
+	PRESS = PRESS * 1000;
+	return PRESS;
+}
+
+double tonm_to_newtoncm(double PRESS) {
+	PRESS = PRESS * 0.9807;
+	return PRESS;
+}
+
+double tonm_to_newtonm(double PRESS) {
+	PRESS = PRESS * 9807;
+	return PRESS;
+}
+//newtoncm
+double newtoncm_to_bar(double PRESS) {
+	PRESS = PRESS / 10;
+	return PRESS;
+}
+
+double newtoncm_to_mbar(double PRESS) {
+	PRESS = PRESS * 100;
+	return PRESS;
+}
+
+double newtoncm_to_paskal(double PRESS) {
+	PRESS = PRESS * 10000;
+	return PRESS;
+}
+
+double newtoncm_to_kpaskal(double PRESS) {
+	PRESS = PRESS * 10;
+	return PRESS;
+}
+
+double newtoncm_to_Mpaskal(double PRESS) {
+	PRESS = PRESS / 100;
+	return PRESS;
+}
+
+double newtoncm_to_gcm(double PRESS) {
+	PRESS = PRESS * 100;
+	return PRESS;
+}
+
+double newtoncm_to_kgcm(double PRESS) {
+	PRESS = PRESS * 102;
+	return PRESS;
+}
+
+double newtoncm_to_toncm(double PRESS) {
+	PRESS = PRESS * 0.000102;
+	return PRESS;
+}
+
+double newtoncm_to_kgm(double PRESS) {
+	PRESS = PRESS * 1020;
+	return PRESS;
+}
+
+double newtoncm_to_tonm(double PRESS) {
+	PRESS = PRESS * 1.02;
+	return PRESS;
+}
+
+double newtoncm_to_newtonm(double PRESS) {
+	PRESS = PRESS * 10000;
+	return PRESS;
+}
+//newtonm
+double newtonm_to_bar(double PRESS) {
+	PRESS = PRESS / 100000;
+	return PRESS;
+}
+
+double newtonm_to_mbar(double PRESS) {
+	PRESS = PRESS / 100;
+	return PRESS;
+}
+
+double newtonm_to_paskal(double PRESS) {
+	
+	return PRESS;
+}
+
+double newtonm_to_kpaskal(double PRESS) {
+	PRESS = PRESS / 1000;
+	return PRESS;
+}
+
+double newtonm_to_Mpaskal(double PRESS) {
+	PRESS = PRESS / 1000000;
+	return PRESS;
+}
+
+double newtonm_to_gcm(double PRESS) {
+	PRESS = PRESS * 0.0102;
+	return PRESS;
+}
+
+double newtonm_to_kgcm(double PRESS) {
+	PRESS = PRESS * 0.0000102;
+	return PRESS;
+}
+
+double newtonm_to_toncm(double PRESS) {
+	PRESS = PRESS * 0.000102;
+	return PRESS;
+}
+
+double newtonm_to_kgm(double PRESS) {
+	PRESS = PRESS * 0.0000000102;
+	return PRESS;
+}
+
+double newtonm_to_tonm(double PRESS) {
+	PRESS = PRESS * 0.000102;
+	return PRESS;
+}
+
+double newtonm_to_newtoncm(double PRESS) {
+	PRESS = PRESS / 10000;
+	return PRESS;
+}
