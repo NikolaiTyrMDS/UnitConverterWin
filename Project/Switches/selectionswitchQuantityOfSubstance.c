07e0cbee5066f2a99dcc17aff53526cc77384746
ProkopenkoRoman <long399@mail.ru>
2016-05-16 22:44:25 +0600

    Update selectionswitchQuantityOfSubstance.c

--- Project/Switches/selectionswitchQuantityOfSubstance.c
+++ Project/Switches/selectionswitchQuantityOfSubstance.c
@@ -1,37 +1,39 @@
 #include <stdio.h>
 #include <math.h>
-#include"../Headers/total.h" /* Символ '../' - escape-символ - используется для перехода на один уровень выше;
-                                Этот символ используется столько раз, на сколько уровней нужно подняться; */
+#include"../Headers/total.h" /* РЎРёРјРІРѕР» '../' - escape-СЃРёРјРІРѕР» - РёСЃРїРѕР»СЊР·СѓРµС‚СЃСЏ РґР»СЏ РїРµСЂРµС…РѕРґР° РЅР° РѕРґРёРЅ СѓСЂРѕРІРµРЅСЊ РІС‹С€Рµ;
+                                Р­С‚РѕС‚ СЃРёРјРІРѕР» РёСЃРїРѕР»СЊР·СѓРµС‚СЃСЏ СЃС‚РѕР»СЊРєРѕ СЂР°Р·, РЅР° СЃРєРѕР»СЊРєРѕ СѓСЂРѕРІРЅРµР№ РЅСѓР¶РЅРѕ РїРѕРґРЅСЏС‚СЊСЃСЏ; */
 
 double selectionswitchQuantityOfSubstance(double MEASUREMENT)
 {
     FunctionsParameters key;
 
-    printf("Выберите величину, из которой Вы хотите произвести конвертирование:\n"
-           "1 - Микромолей;\n"
-           "2 - Миллимолей;\n"
-           "3 - Молей;\n"
-           "4 - Киломолей;\n"
-           "5 - Мегамолей;\n"
-           "6 - Фунт-молей;\n\t");
-    scanf("%d", &key.input_choice);
-    while(key.input_choice >= 10 || key.input_choice == 0) {
-        fprintf(stderr, "\nВы выбрали величину, которой нет в списке доступных величин. \nПожалуйста, сделайте корректный выбор\n\t");
+    printf("Р’С‹Р±РµСЂРёС‚Рµ РІРµР»РёС‡РёРЅСѓ, РёР· РєРѕС‚РѕСЂРѕР№ Р’С‹ С…РѕС‚РёС‚Рµ РїСЂРѕРёР·РІРµСЃС‚Рё РєРѕРЅРІРµСЂС‚РёСЂРѕРІР°РЅРёРµ:\n"
+           "1 - РњРёРєСЂРѕРјРѕР»РµР№;\n"
+           "2 - РњРёР»Р»РёРјРѕР»РµР№;\n"
+           "3 - РњРѕР»РµР№;\n"
+           "4 - РљРёР»РѕРјРѕР»РµР№;\n"
+           "5 - РњРµРіР°РјРѕР»РµР№;\n"
+           "6 - Р¤СѓРЅС‚-РјРѕР»РµР№;\n\t");
+    key.input_choice = parsing_id(key.in);
+    //scanf("%d", &key.input_choice);
+    while(key.input_choice >= 7 || key.input_choice == 0) {
+        fprintf(stderr, "\nР’С‹ РІС‹Р±СЂР°Р»Рё РІРµР»РёС‡РёРЅСѓ, РєРѕС‚РѕСЂРѕР№ РЅРµС‚ РІ СЃРїРёСЃРєРµ РґРѕСЃС‚СѓРїРЅС‹С… РІРµР»РёС‡РёРЅ. \nРџРѕР¶Р°Р»СѓР№СЃС‚Р°, СЃРґРµР»Р°Р№С‚Рµ РєРѕСЂСЂРµРєС‚РЅС‹Р№ РІС‹Р±РѕСЂ\n\t");
         scanf("%d", &key.input_choice);
     }
 
-    printf("Выберите величину, в которую Вы хотите конвертировать Вашу величину.\n"
-           "Не нужно выбирать ту же величину, что Вы выбрали в качестве конвертируемой:\n"
-           "1 - Микромолей;\n"
-           "2 - Миллимолей;\n"
-           "3 - Молей;\n"
-           "4 - Киломолей;\n"
-           "5 - Мегамолей;\n"
-           "6 - Фунт-молей;\n\t");
-    scanf("%d", &key.output_choice);
-    while(key.output_choice == key.input_choice || key.input_choice >= 10 || key.input_choice == 0 )  {
-        fprintf(stderr, "\nВы выбрали величину, которой нет в списке доступных величин, \nили Вы пытаетесь конвертировать "
-                "одну и ту же величину. \nПожалуйста, сделайте корректный выбор\n\t");
+    printf("Р’С‹Р±РµСЂРёС‚Рµ РІРµР»РёС‡РёРЅСѓ, РІ РєРѕС‚РѕСЂСѓСЋ Р’С‹ С…РѕС‚РёС‚Рµ РєРѕРЅРІРµСЂС‚РёСЂРѕРІР°С‚СЊ Р’Р°С€Сѓ РІРµР»РёС‡РёРЅСѓ.\n"
+           "РќРµ РЅСѓР¶РЅРѕ РІС‹Р±РёСЂР°С‚СЊ С‚Сѓ Р¶Рµ РІРµР»РёС‡РёРЅСѓ, С‡С‚Рѕ Р’С‹ РІС‹Р±СЂР°Р»Рё РІ РєР°С‡РµСЃС‚РІРµ РєРѕРЅРІРµСЂС‚РёСЂСѓРµРјРѕР№:\n"
+           "1 - РњРёРєСЂРѕРјРѕР»РµР№;\n"
+           "2 - РњРёР»Р»РёРјРѕР»РµР№;\n"
+           "3 - РњРѕР»РµР№;\n"
+           "4 - РљРёР»РѕРјРѕР»РµР№;\n"
+           "5 - РњРµРіР°РјРѕР»РµР№;\n"
+           "6 - Р¤СѓРЅС‚-РјРѕР»РµР№;\n\t");
+    key.output_choice = parsing_id(key.out);
+    //scanf("%d", &key.output_choice);
+    while(key.output_choice == key.input_choice || key.input_choice >= 7 || key.input_choice == 0 )  {
+        fprintf(stderr, "\nР’С‹ РІС‹Р±СЂР°Р»Рё РІРµР»РёС‡РёРЅСѓ, РєРѕС‚РѕСЂРѕР№ РЅРµС‚ РІ СЃРїРёСЃРєРµ РґРѕСЃС‚СѓРїРЅС‹С… РІРµР»РёС‡РёРЅ, \nРёР»Рё Р’С‹ РїС‹С‚Р°РµС‚РµСЃСЊ РєРѕРЅРІРµСЂС‚РёСЂРѕРІР°С‚СЊ "
+                "РѕРґРЅСѓ Рё С‚Сѓ Р¶Рµ РІРµР»РёС‡РёРЅСѓ. \nРџРѕР¶Р°Р»СѓР№СЃС‚Р°, СЃРґРµР»Р°Р№С‚Рµ РєРѕСЂСЂРµРєС‚РЅС‹Р№ РІС‹Р±РѕСЂ\n\t");
         scanf("%d", &key.output_choice);
     }
 
@@ -39,34 +41,34 @@ double selectionswitchQuantityOfSubstance(double MEASUREMENT)
 
     switch(key.id) {
         case 12:
-            key.gate = MkMole2mMole(MEASUREMENT);
+            key.gate = Mole2mMole(MkMole2Mole(MEASUREMENT));
             break;
         case 13:
             key.gate = MkMole2Mole(MEASUREMENT);
             break;
         case 14:
-            key.gate = MkMole2kMole(MEASUREMENT);
+            key.gate = Mole2kMole(MkMole2Mole(MEASUREMENT));
             break;
         case 15:
-            key.gate = MkMole2MgMole(MEASUREMENT);
+            key.gate = Mole2MgMole(MkMole2Mole(MEASUREMENT));
             break;
         case 16:
-            key.gate = MkMole2PoundMole(MEASUREMENT);
+            key.gate = Mole2PoundMole(MkMole2Mole(MEASUREMENT));
             break;
         case 21:
-            key.gate = mMole2MkMole(MEASUREMENT);
+            key.gate = Mole2MkMole(mMole2Mole(MEASUREMENT));
             break;
         case 23:
             key.gate = mMole2Mole(MEASUREMENT);
             break;
         case 24:
-            key.gate = mMole2kMole(MEASUREMENT);
+            key.gate = Mole2kMole(mMole2Mole(MEASUREMENT));
             break;
         case 25:
-            key.gate = mMole2MgMole(MEASUREMENT);
+            key.gate = Mole2MgMole(mMole2Mole(MEASUREMENT));
             break;
         case 26:
-            key.gate = mMole2PoundMole(MEASUREMENT);
+            key.gate = Mole2PoundMole(mMole2Mole(MEASUREMENT));
             break;
         case 31:
             key.gate = Mole2MkMole(MEASUREMENT);
@@ -84,53 +86,53 @@ double selectionswitchQuantityOfSubstance(double MEASUREMENT)
             key.gate = Mole2PoundMole(MEASUREMENT);
             break;
         case 41:
-            key.gate = kMole2MkMole(MEASUREMENT);
+            key.gate = Mole2MkMole(kMole2Mole(MEASUREMENT));
             break;
         case 42:
-            key.gate = kMole2mMole(MEASUREMENT);
+            key.gate = Mole2mMole(kMole2Mole(MEASUREMENT));
             break;
         case 43:
             key.gate = kMole2Mole(MEASUREMENT);
             break;
         case 45:
-            key.gate = kMole2MgMole(MEASUREMENT);
+            key.gate = Mole2MgMole(kMole2Mole(MEASUREMENT));
             break;
         case 46:
-            key.gate = kMole2PoundMole(MEASUREMENT);
+            key.gate = Mole2PoundMole(kMole2Mole(MEASUREMENT));
             break;
         case 51:
-            key.gate = MgMole2MkMole(MEASUREMENT);
+            key.gate = Mole2MkMole(MgMole2Mole(MEASUREMENT));
             break;
         case 52:
-            key.gate = MgMole2mMole(MEASUREMENT);
+            key.gate = Mole2mMole(MgMole2Mole(MEASUREMENT));
             break;
         case 53:
             key.gate = MgMole2Mole(MEASUREMENT);
             break;
         case 54:
-            key.gate = MgMole2kMole(MEASUREMENT);
+            key.gate = Mole2kMole(MgMole2Mole(MEASUREMENT));
             break;
         case 56:
-            key.gate = MgMole2PoundMole(MEASUREMENT);
+            key.gate = Mole2PoundMole(MgMole2Mole(MEASUREMENT));
             break;
         case 61:
-            key.gate = PoundMole2MkMole(MEASUREMENT);
+            key.gate = Mole2MkMole(PoundMole2Mole(MEASUREMENT));
             break;
         case 62:
-            key.gate = PoundMole2mMole(MEASUREMENT);
+            key.gate = Mole2mMole(PoundMole2Mole(MEASUREMENT));
             break;
         case 63:
             key.gate = PoundMole2Mole(MEASUREMENT);
             break;
         case 64:
-            key.gate = PoundMole2kMole(MEASUREMENT);
+            key.gate = Mole2kMole(PoundMole2Mole(MEASUREMENT));
             break;
         case 65:
-            key.gate = PoundMole2MgMole(MEASUREMENT);
+            key.gate = Mole2MgMole(PoundMole2Mole(MEASUREMENT));
             break;
         default:
-            printf("Не введено значение конвертируемой величины, "
-                   "или Вы пытаетесь конвертировать в величину, которую уже преобразуете\n");
+            printf("РќРµ РІРІРµРґРµРЅРѕ Р·РЅР°С‡РµРЅРёРµ РєРѕРЅРІРµСЂС‚РёСЂСѓРµРјРѕР№ РІРµР»РёС‡РёРЅС‹, "
+                   "РёР»Рё Р’С‹ РїС‹С‚Р°РµС‚РµСЃСЊ РєРѕРЅРІРµСЂС‚РёСЂРѕРІР°С‚СЊ РІ РІРµР»РёС‡РёРЅСѓ, РєРѕС‚РѕСЂСѓСЋ СѓР¶Рµ РїСЂРµРѕР±СЂР°Р·СѓРµС‚Рµ\n");
             break;
     }
 
