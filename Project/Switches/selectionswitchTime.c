f03df42703522811813fde8826e61f5a20b2c56e
PopovMihail <popov-mm@ya.ru>
2016-05-17 16:33:58 +0700

    изменил согласно сокрощённому модулю

--- Project/Switches/selectionswitchTime.c
+++ Project/Switches/selectionswitchTime.c
@@ -1,161 +1,163 @@
 #include <stdio.h>
 #include <math.h>
-#include "../Headers/total.h" /* Символ '../' - escape-символ - используется для перехода на один уровень выше;
-                                Этот символ используется столько раз, на сколько уровней нужно подняться; */
+#include "../Headers/total.h" /* РЎРёРјРІРѕР» '../' - escape-СЃРёРјРІРѕР» - РёСЃРїРѕР»СЊР·СѓРµС‚СЃСЏ РґР»СЏ РїРµСЂРµС…РѕРґР° РЅР° РѕРґРёРЅ СѓСЂРѕРІРµРЅСЊ РІС‹С€Рµ;
+                                Р­С‚РѕС‚ СЃРёРјРІРѕР» РёСЃРїРѕР»СЊР·СѓРµС‚СЃСЏ СЃС‚РѕР»СЊРєРѕ СЂР°Р·, РЅР° СЃРєРѕР»СЊРєРѕ СѓСЂРѕРІРЅРµР№ РЅСѓР¶РЅРѕ РїРѕРґРЅСЏС‚СЊСЃСЏ; */
 
 double selectionswitchTime(double MEASUREMENT)
 {
     FunctionsParameters key;
 
-    printf("Выберите величину, из которой Вы хотите произвести конвертирование:\n"
-           " 1 - Наносекунды;\n"
-           " 2 - Микросекунды;\n"
-           " 3 - Миллисекунды;\n"
-           " 4 - Секунды;\n"
-           " 5 - Минуты;\n"
-           " 6 - Часы;\n"
-           " 7 - Сутки;\n"
-           " 8 - Недели;\n"
-           " 9 - Года;\n"
-           "10 - Века;\n \t");
-    scanf("%d", &key.input_choice);
-    while(key.input_choice >= 10 || key.input_choice == 0) {
-        fprintf(stderr, "\nВы выбрали величину, которой нет в списке доступных величин. \nПожалуйста, сделайте корректный выбор\n\t");
+    printf("Р’С‹Р±РµСЂРёС‚Рµ РІРµР»РёС‡РёРЅСѓ, РёР· РєРѕС‚РѕСЂРѕР№ Р’С‹ С…РѕС‚РёС‚Рµ РїСЂРѕРёР·РІРµСЃС‚Рё РєРѕРЅРІРµСЂС‚РёСЂРѕРІР°РЅРёРµ:\n"
+           " 1 - РќР°РЅРѕСЃРµРєСѓРЅРґС‹;\n"
+           " 2 - РњРёРєСЂРѕСЃРµРєСѓРЅРґС‹;\n"
+           " 3 - РњРёР»Р»РёСЃРµРєСѓРЅРґС‹;\n"
+           " 4 - РЎРµРєСѓРЅРґС‹;\n"
+           " 5 - РњРёРЅСѓС‚С‹;\n"
+           " 6 - Р§Р°СЃС‹;\n"
+           " 7 - РЎСѓС‚РєРё;\n"
+           " 8 - РќРµРґРµР»Рё;\n"
+           " 9 - Р“РѕРґР°;\n"
+           " 10 - Р’РµРєР°;\n \t");
+    //scanf("%d", &key.input_choice);
+    key.input_choice = parsing_id(key.in);
+    while(key.input_choice > 10 || key.input_choice == 0) {
+        fprintf(stderr, "\nР’С‹ РІС‹Р±СЂР°Р»Рё РІРµР»РёС‡РёРЅСѓ, РєРѕС‚РѕСЂРѕР№ РЅРµС‚ РІ СЃРїРёСЃРєРµ РґРѕСЃС‚СѓРїРЅС‹С… РІРµР»РёС‡РёРЅ. \nРџРѕР¶Р°Р»СѓР№СЃС‚Р°, СЃРґРµР»Р°Р№С‚Рµ РєРѕСЂСЂРµРєС‚РЅС‹Р№ РІС‹Р±РѕСЂ\n\t");
         scanf("%d", &key.input_choice);
     }
 
-    printf("Выберите величину, в которую Вы хотите конвертировать Вашу величину.\n"
-           "Не нужно выбирать ту же величину, что Вы выбрали в качестве конвертируемой:\n"
-           " 1 - Наносекунды;\n"
-           " 2 - Микросекунды;\n"
-           " 3 - Миллисекунды;\n"
-           " 4 - Секунды;\n"
-           " 5 - Минуты;\n"
-           " 6 - Часы;\n"
-           " 7 - Сутки;\n"
-           " 8 - Недели;\n"
-           " 9 - Года;\n"
-           "10 - Века;\n \t");
-    scanf("%d", &key.output_choice);
-    while(key.output_choice == key.input_choice || key.input_choice >= 10 || key.input_choice == 0 )  {
-        fprintf(stderr, "\nВы выбрали величину, которой нет в списке доступных величин, \nили Вы пытаетесь конвертировать "
-                "одну и ту же величину. \nПожалуйста, сделайте корректный выбор\n\t");
+    printf("Р’С‹Р±РµСЂРёС‚Рµ РІРµР»РёС‡РёРЅСѓ, РІ РєРѕС‚РѕСЂСѓСЋ Р’С‹ С…РѕС‚РёС‚Рµ РєРѕРЅРІРµСЂС‚РёСЂРѕРІР°С‚СЊ Р’Р°С€Сѓ РІРµР»РёС‡РёРЅСѓ.\n"
+           "РќРµ РЅСѓР¶РЅРѕ РІС‹Р±РёСЂР°С‚СЊ С‚Сѓ Р¶Рµ РІРµР»РёС‡РёРЅСѓ, С‡С‚Рѕ Р’С‹ РІС‹Р±СЂР°Р»Рё РІ РєР°С‡РµСЃС‚РІРµ РєРѕРЅРІРµСЂС‚РёСЂСѓРµРјРѕР№:\n"
+           " 1 - РќР°РЅРѕСЃРµРєСѓРЅРґС‹;\n"
+           " 2 - РњРёРєСЂРѕСЃРµРєСѓРЅРґС‹;\n"
+           " 3 - РњРёР»Р»РёСЃРµРєСѓРЅРґС‹;\n"
+           " 4 - РЎРµРєСѓРЅРґС‹;\n"
+           " 5 - РњРёРЅСѓС‚С‹;\n"
+           " 6 - Р§Р°СЃС‹;\n"
+           " 7 - РЎСѓС‚РєРё;\n"
+           " 8 - РќРµРґРµР»Рё;\n"
+           " 9 - Р“РѕРґР°;\n"
+           " 10 - Р’РµРєР°;\n \t");
+    //scanf("%d", &key.output_choice);
+    key.output_choice = parsing_id(key.out);
+    while(key.output_choice == key.input_choice || key.input_choice > 10 || key.input_choice == 0 )  {
+        fprintf(stderr, "\nР’С‹ РІС‹Р±СЂР°Р»Рё РІРµР»РёС‡РёРЅСѓ, РєРѕС‚РѕСЂРѕР№ РЅРµС‚ РІ СЃРїРёСЃРєРµ РґРѕСЃС‚СѓРїРЅС‹С… РІРµР»РёС‡РёРЅ, \nРёР»Рё Р’С‹ РїС‹С‚Р°РµС‚РµСЃСЊ РєРѕРЅРІРµСЂС‚РёСЂРѕРІР°С‚СЊ "
+                "РѕРґРЅСѓ Рё С‚Сѓ Р¶Рµ РІРµР»РёС‡РёРЅСѓ. \nРџРѕР¶Р°Р»СѓР№СЃС‚Р°, СЃРґРµР»Р°Р№С‚Рµ РєРѕСЂСЂРµРєС‚РЅС‹Р№ РІС‹Р±РѕСЂ\n\t");
         scanf("%d", &key.output_choice);
     }
 
     key.id = key.input_choice * 100 + key.output_choice;
 
-    printf("ID равно %d", key.id);
+    //printf("ID СЂР°РІРЅРѕ %d", key.id);
 
     switch(key.id) {
         case 102:
-            key.gate = nsec_to_mksec(MEASUREMENT);
+            key.gate = min_to_mksec(nsec_to_min(MEASUREMENT));
             break;
         case 103:
-            key.gate = nsec_to_msec(MEASUREMENT);
+            key.gate = min_to_msec(nsec_to_min(MEASUREMENT));
             break;
         case 104:
-            key.gate = nsec_to_sec(MEASUREMENT);
+            key.gate = min_to_sec(nsec_to_min(MEASUREMENT));
             break;
         case 105:
             key.gate = nsec_to_min(MEASUREMENT);
             break;
         case 106:
-            key.gate = nsec_to_chas(MEASUREMENT);
+            key.gate = min_to_chas(nsec_to_min(MEASUREMENT));
             break;
         case 107:
-            key.gate = nsec_to_sut(MEASUREMENT);
+            key.gate = min_to_sut(nsec_to_min(MEASUREMENT));
             break;
         case 108:
-            key.gate = nsec_to_week(MEASUREMENT);
+            key.gate = min_to_week(nsec_to_min(MEASUREMENT));
             break;
         case 109:
-            key.gate = nsec_to_year(MEASUREMENT);
+            key.gate = min_to_year(nsec_to_min(MEASUREMENT));
             break;
         case 110:
-            key.gate = nsec_to_vek(MEASUREMENT);
+            key.gate = min_to_vek(nsec_to_min(MEASUREMENT));
             break;
         case 201:
-            key.gate = mksec_to_nsec(MEASUREMENT);
+            key.gate = min_to_nsec(mksec_to_min(MEASUREMENT));
             break;
         case 203:
-            key.gate = mksec_to_msec(MEASUREMENT);
+            key.gate = min_to_msec(mksec_to_min(MEASUREMENT));
             break;
         case 204:
-            key.gate = mksec_to_sec(MEASUREMENT);
+            key.gate = min_to_sec(mksec_to_min(MEASUREMENT));
             break;
         case 205:
             key.gate = mksec_to_min(MEASUREMENT);
             break;
         case 206:
-            key.gate = mksec_to_chas(MEASUREMENT);
+            key.gate = min_to_chas(mksec_to_min(MEASUREMENT));
             break;
         case 207:
-            key.gate = mksec_to_sut(MEASUREMENT);
+            key.gate = min_to_sut(mksec_to_min(MEASUREMENT));
             break;
         case 208:
-            key.gate = mksec_to_week(MEASUREMENT);
+            key.gate = min_to_week(mksec_to_min(MEASUREMENT));
             break;
         case 209:
-            key.gate = mksec_to_year(MEASUREMENT);
+            key.gate = min_to_year(mksec_to_min(MEASUREMENT));
             break;
         case 210:
-            key.gate = mksec_to_vek(MEASUREMENT);
+            key.gate = min_to_vek(mksec_to_min(MEASUREMENT));
             break;
         case 301:
-            key.gate = msec_to_nsec(MEASUREMENT);
+            key.gate = min_to_nsec(msec_to_min(MEASUREMENT));
             break;
         case 302:
-            key.gate = msec_to_mksec(MEASUREMENT);
+            key.gate = min_to_mksec(msec_to_min(MEASUREMENT));
             break;
         case 304:
-            key.gate = msec_to_sec(MEASUREMENT);
+            key.gate = min_to_sec(msec_to_min(MEASUREMENT));
             break;
         case 305:
             key.gate = msec_to_min(MEASUREMENT);
             break;
         case 306:
-            key.gate = msec_to_chas(MEASUREMENT);
+            key.gate = min_to_chas(msec_to_min(MEASUREMENT));
             break;
         case 307:
-            key.gate = msec_to_sut(MEASUREMENT);
+            key.gate = min_to_sut(msec_to_min(MEASUREMENT));
             break;
         case 308:
-            key.gate = msec_to_week(MEASUREMENT);
+            key.gate = min_to_week(msec_to_min(MEASUREMENT));
             break;
         case 309:
-            key.gate = msec_to_year(MEASUREMENT);
+            key.gate = min_to_year(msec_to_min(MEASUREMENT));
             break;
         case 310:
-            key.gate = msec_to_vek(MEASUREMENT);
+            key.gate = min_to_vek(msec_to_min(MEASUREMENT));
             break;
         case 401:
-            key.gate = sec_to_nsec(MEASUREMENT);
+            key.gate = min_to_nsec(sec_to_min(MEASUREMENT));
             break;
         case 402:
-            key.gate = sec_to_mksec(MEASUREMENT);
+            key.gate = min_to_mksec(sec_to_min(MEASUREMENT));
             break;
         case 403:
-            key.gate = sec_to_msec(MEASUREMENT);
+            key.gate = min_to_msec(sec_to_min(MEASUREMENT));
             break;
         case 405:
             key.gate = sec_to_min(MEASUREMENT);
             break;
         case 406:
-            key.gate = sec_to_chas(MEASUREMENT);
+            key.gate = min_to_chas(sec_to_min(MEASUREMENT));
             break;
         case 407:
-            key.gate = sec_to_sut(MEASUREMENT);
+            key.gate = min_to_sut(sec_to_min(MEASUREMENT));
             break;
         case 408:
-            key.gate = sec_to_week(MEASUREMENT);
+            key.gate = min_to_week(sec_to_min(MEASUREMENT));
             break;
         case 409:
-            key.gate = sec_to_year(MEASUREMENT);
+            key.gate = min_to_year(sec_to_min(MEASUREMENT));
             break;
-       /* case 410:
-            key.gate = sec_to_vek(MEASUREMENT);
-            break;*/
+        case 410:
+			key.gate = min_to_vek(sec_to_min(MEASUREMENT));
+            break;
         case 501:
             key.gate = min_to_nsec(MEASUREMENT);
             break;
@@ -184,143 +186,143 @@ double selectionswitchTime(double MEASUREMENT)
             key.gate = min_to_vek(MEASUREMENT);
             break;
         case 601:
-            key.gate = chas_to_nsec(MEASUREMENT);
+            key.gate = min_to_nsec(chas_to_min(MEASUREMENT));
             break;
         case 602:
-            key.gate = chas_to_mksec(MEASUREMENT);
+            key.gate = min_to_mksec(chas_to_min(MEASUREMENT));
             break;
         case 603:
-            key.gate = chas_to_msec(MEASUREMENT);
+            key.gate = min_to_msec(chas_to_min(MEASUREMENT));
             break;
         case 604:
-            key.gate = chas_to_sec(MEASUREMENT);
+            key.gate = min_to_sec(chas_to_min(MEASUREMENT));
             break;
         case 605:
             key.gate = chas_to_min(MEASUREMENT);
             break;
         case 607:
-            key.gate = chas_to_sut(MEASUREMENT);
+            key.gate = min_to_sut(chas_to_min(MEASUREMENT));
             break;
         case 608:
-            key.gate = chas_to_week(MEASUREMENT);
+            key.gate = min_to_week(chas_to_min(MEASUREMENT));
             break;
         case 609:
-            key.gate = chas_to_year(MEASUREMENT);
+            key.gate = min_to_year(chas_to_min(MEASUREMENT));
             break;
         case 610:
-            key.gate = chas_to_vek(MEASUREMENT);
+            key.gate = min_to_vek(chas_to_min(MEASUREMENT));
             break;
         case 701:
-            key.gate = sut_to_nsec(MEASUREMENT);
+            key.gate = min_to_nsec(sut_to_min(MEASUREMENT));
             break;
         case 702:
-            key.gate = sut_to_mksec(MEASUREMENT);
+            key.gate = min_to_mksec(sut_to_min(MEASUREMENT));
             break;
         case 703:
-            key.gate = sut_to_msec(MEASUREMENT);
+            key.gate = min_to_msec(sut_to_min(MEASUREMENT));
             break;
         case 704:
-            key.gate = sut_to_sec(MEASUREMENT);
+            key.gate = min_to_sec(sut_to_min(MEASUREMENT));
             break;
         case 705:
             key.gate = sut_to_min(MEASUREMENT);
             break;
         case 706:
-            key.gate = sut_to_chas(MEASUREMENT);
+            key.gate = min_to_chas(sut_to_min(MEASUREMENT));
             break;
         case 708:
-            key.gate = sut_to_week(MEASUREMENT);
+            key.gate = min_to_week(sut_to_min(MEASUREMENT));
             break;
         case 709:
-            key.gate = sut_to_year(MEASUREMENT);
+            key.gate = min_to_year(sut_to_min(MEASUREMENT));
             break;
         case 710:
-            key.gate = sut_to_vek(MEASUREMENT);
+            key.gate = min_to_vek(sut_to_min(MEASUREMENT));
             break;
         case 801:
-            key.gate = week_to_nsec(MEASUREMENT);
+            key.gate = min_to_nsec(week_to_min(MEASUREMENT));
             break;
         case 802:
-            key.gate = week_to_mksec(MEASUREMENT);
+            key.gate = min_to_mksec(week_to_min(MEASUREMENT));
             break;
         case 803:
-            key.gate = week_to_msec(MEASUREMENT);
+            key.gate = min_to_msec(week_to_min(MEASUREMENT));
             break;
         case 804:
-            key.gate = week_to_sec(MEASUREMENT);
+            key.gate = min_to_sec(week_to_min(MEASUREMENT));
             break;
         case 805:
             key.gate = week_to_min(MEASUREMENT);
             break;
         case 806:
-            key.gate = week_to_chas(MEASUREMENT);
+            key.gate = min_to_chas(week_to_min(MEASUREMENT));
             break;
         case 807:
-            key.gate = week_to_sut(MEASUREMENT);
+            key.gate = min_to_sut(week_to_min(MEASUREMENT));
             break;
         case 809:
-            key.gate = week_to_year(MEASUREMENT);
+            key.gate = min_to_year(week_to_min(MEASUREMENT));
             break;
         case 810:
-            key.gate = week_to_vek(MEASUREMENT);
+            key.gate = min_to_vek(week_to_min(MEASUREMENT));
             break;
         case 901:
-            key.gate = year_to_nsec(MEASUREMENT);
+            key.gate = min_to_nsec(year_to_min(MEASUREMENT));
             break;
         case 902:
-            key.gate = year_to_mksec(MEASUREMENT);
+            key.gate = min_to_mksec(year_to_min(MEASUREMENT));
             break;
         case 903:
-            key.gate = year_to_msec(MEASUREMENT);
+            key.gate = min_to_msec(year_to_min(MEASUREMENT));
             break;
         case 904:
-            key.gate = year_to_sec(MEASUREMENT);
+            key.gate = min_to_sec(year_to_min(MEASUREMENT));
             break;
         case 905:
             key.gate = year_to_min(MEASUREMENT);
             break;
         case 906:
-            key.gate = year_to_chas(MEASUREMENT);
+            key.gate = min_to_chas(year_to_min(MEASUREMENT));
             break;
         case 907:
-            key.gate = year_to_sut(MEASUREMENT);
+            key.gate = min_to_sut(year_to_min(MEASUREMENT));
             break;
         case 908:
-            key.gate = year_to_week(MEASUREMENT);
+            key.gate = min_to_week(year_to_min(MEASUREMENT));
             break;
         case 910:
-            key.gate = year_to_vek(MEASUREMENT);
+            key.gate = min_to_vek(year_to_min(MEASUREMENT));
             break;
         case 1001:
-            key.gate = vek_to_nsec(MEASUREMENT);
+            key.gate = min_to_nsec(vek_to_min(MEASUREMENT));
             break;
         case 1002:
-            key.gate = vek_to_mksec(MEASUREMENT);
+            key.gate = min_to_mksec(vek_to_min(MEASUREMENT));
             break;
         case 1003:
-            key.gate = vek_to_msec(MEASUREMENT);
+            key.gate = min_to_msec(vek_to_min(MEASUREMENT));
             break;
         case 1004:
-            key.gate = vek_to_sec(MEASUREMENT);
+            key.gate = min_to_sec(vek_to_min(MEASUREMENT));
             break;
         case 1005:
             key.gate = vek_to_min(MEASUREMENT);
             break;
         case 1006:
-            key.gate = vek_to_chas(MEASUREMENT);
+            key.gate = min_to_chas(vek_to_min(MEASUREMENT));
             break;
         case 1007:
-            key.gate = vek_to_sut(MEASUREMENT);
+            key.gate = min_to_sut(vek_to_min(MEASUREMENT));
             break;
         case 1008:
-            key.gate = vek_to_week(MEASUREMENT);
+            key.gate = min_to_week(vek_to_min(MEASUREMENT));
             break;
         case 1009:
-            key.gate = vek_to_year(MEASUREMENT);
+            key.gate = min_to_year(vek_to_min(MEASUREMENT));
             break;
         default:
-            printf("Не введено значение конвертируемой величины, "
-                   "или Вы пытаетесь конвертировать в величину, которую уже преобразуете\n");
+            printf("РќРµ РІРІРµРґРµРЅРѕ Р·РЅР°С‡РµРЅРёРµ РєРѕРЅРІРµСЂС‚РёСЂСѓРµРјРѕР№ РІРµР»РёС‡РёРЅС‹, "
+                   "РёР»Рё Р’С‹ РїС‹С‚Р°РµС‚РµСЃСЊ РєРѕРЅРІРµСЂС‚РёСЂРѕРІР°С‚СЊ РІ РІРµР»РёС‡РёРЅСѓ, РєРѕС‚РѕСЂСѓСЋ СѓР¶Рµ РїСЂРµРѕР±СЂР°Р·СѓРµС‚Рµ\n");
             break;
     }
 
