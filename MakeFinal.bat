0716ca0bf895f60085fd3f5ecc2c5548f647a98c
NikolaiTyrMASSADA <NEWTYPEDEITY@live.ru>
2016-05-21 11:37:42 +0600

    Deleted extra files

--- MakeFinal.bat
+++
@@ -1,3 +0,0 @@
-gcc.exe -Wall -o D:\Documents\GitHub\UNIT_Converter\Converter.exe D:\Documents\GitHub\UNIT_Converter\Project\Modules\Data.c D:\Documents\GitHub\UNIT_Converter\Project\Modules\Main.c D:\Documents\GitHub\UNIT_Converter\Project\Switches\selectionswitchData.c
-pause
-exit
