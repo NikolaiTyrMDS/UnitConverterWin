5531aa01911a6b0939383d83910a4d009d5a1a84
Nerevrin <Romanin97@yandex.ru>
2016-05-31 17:22:45 +0600

    Update Strength.c

--- Project/Modules/Strength.c
+++ Project/Modules/Strength.c
@@ -1,255 +1,78 @@
 #include <stdio.h>
 // DONE;
-/* ФУНКЦИИ КОНВЕРТИРОВАНИЯ РАЗЛИЧНЫХ ЕДИНИЦ ИЗМЕРЕНИЯ СИЛЫ */
+/* Р¤РЈРќРљР¦РР РљРћРќР’Р•Р РўРР РћР’РђРќРРЇ Р РђР—Р›РР§РќР«РҐ Р•Р”РРќРР¦ РР—РњР•Р Р•РќРРЇ РЎРР›Р« */
 
-// <Ньютоны> ---> <Килоньютоны> //
+// <РќСЊСЋС‚РѕРЅ> ---> <РљРёР»РѕРЅСЊСЋС‚РѕРЅ> //
 double Newton2kNewton(double STRENGTH) {
 	STRENGTH = STRENGTH / 1000;
 	return STRENGTH;
 }
 
-// <Килоньютоны> ---> <Ньютоны> //
-double kNewton2Newton(double STRENGTH) {
-	STRENGTH = STRENGTH * 1000;
-	return STRENGTH;
-}
 
-// <Ньютоны> ---> <Миллиньютоны> //
+// <РќСЊСЋС‚РѕРЅ> ---> <РњРёР»Р»РёРЅСЊСЋС‚РѕРЅ> //
 double Newton2mNewton(double STRENGTH) {
 	STRENGTH = STRENGTH * 1000;
 	return STRENGTH;
 }
 
-// <Миллиньютоны> ---> <Ньютоны> //
-double mNewton2Newton(double STRENGTH) {
-	STRENGTH = STRENGTH / 1000;
-	return STRENGTH;
-}
-
-// <Миллиньютоны> ---> <Килоньютоны> //
-double mNewton2kNewton(double STRENGTH) {
-	STRENGTH = STRENGTH / 1000000;
-	return STRENGTH;
-}
-
-// <Килоньютоны> ---> <Миллиньютоны> //
-double kNewton2mNewton(double STRENGTH) {
-	STRENGTH = STRENGTH * 1000000;
-	return STRENGTH;
-}
-
-// <Ньютоны> ---> <Меганьютоны> //
+// <РќСЊСЋС‚РѕРЅ> ---> <РњРµРіР°РЅСЊСЋС‚РѕРЅ> //
 double Newton2MgNewton(double STRENGTH) {
 	STRENGTH = STRENGTH / 1000000;
 	return STRENGTH;
 }
 
-// <Меганьютоны> ---> <Ньютоны> //
-double MgNewton2Newton(double STRENGTH) {
-	STRENGTH = STRENGTH * 1000000;
-	return STRENGTH;
-}
-
-// <Миллиньютоны> ---> <Меганьютоны> //
-double mNewton2MgNewton(double STRENGTH) {
-	STRENGTH = STRENGTH / 1000000000;
-	return STRENGTH;
-}
-
-// <Меганьютоны> ---> <Миллиньютоны> //
-double MgNewton2mNewton(double STRENGTH) {
-	STRENGTH = STRENGTH * 1000000000;
-	return STRENGTH;
-}
-
-// <Килоньютоны> ---> <Меганьютоны> //
-double kNewton2MgNewton(double STRENGTH) {
-	STRENGTH = STRENGTH / 1000;
-	return STRENGTH;
-}
-
-// <Меганьютоны> ---> <Килоньютоны> //
-double MgNewton2kNewton(double STRENGTH) {
-	STRENGTH = STRENGTH * 1000;
-	return STRENGTH;
-}
-
-// <Ньютоны> ---> <Микроньютоны> //
+// <РќСЊСЋС‚РѕРЅ> ---> <РњРёРєСЂРѕРЅСЊСЋС‚РѕРЅ> //
 double Newton2MkNewton(double STRENGTH) {
 	STRENGTH = STRENGTH * 1000000;
 	return STRENGTH;
 }
 
-// <Микроньютоны> ---> <Ньютоны> //
-double MkNewton2Newton(double STRENGTH) {
-	STRENGTH = STRENGTH / 1000000;
-	return STRENGTH;
-}
 
-// <Миллиньютоны> ---> <Микроньютоны> //
-double mNewton2MkNewton(double STRENGTH) {
-	STRENGTH = STRENGTH / 1000;
-	return STRENGTH;
-}
-
-// <Микроньютоны> ---> <Миллиньютоны> //
-double MkNewton2mNewton(double STRENGTH) {
-	STRENGTH = STRENGTH * 1000;
-	return STRENGTH;
-}
-
-// <Килоньютоны> ---> <Микроньютоны> //
-double kNewton2MkNewton(double STRENGTH) {
-	STRENGTH = STRENGTH * 1000000000;
-	return STRENGTH;
-}
-
-// <Микроньютоны> ---> <Килоньютоны> //
-double MkNewton2kNewton(double STRENGTH) {
-	STRENGTH = STRENGTH / 1000000000;
-	return STRENGTH;
-}
-
-// <Меганьютоны> ---> <Микроньютоны> //
-double MgNewton2MkNewton(double STRENGTH) {
-	STRENGTH = STRENGTH * 1000000000000;
-	return STRENGTH;
-}
-
-// <Микроньютоны> ---> <Меганьютоны> //
-double MkNewton2MgNewton(double STRENGTH) {
-	STRENGTH = STRENGTH / 1000000000000;
-	return STRENGTH;
-}
-
-// <Ньютоны> ---> <Килограмм-силы> //
+// <РќСЊСЋС‚РѕРЅ> ---> <РљРёР»РѕРіСЂР°РјРј-СЃРёР»Р°> //
 double Newton2kgsNewton(double STRENGTH) {
 	STRENGTH = STRENGTH * 0.101972;
 	return STRENGTH;
 }
 
-// <Килограмм-силы> ---> <Ньютоны> //
-double kgsNewton2Newton(double STRENGTH) {
-	STRENGTH = STRENGTH / 0.101972;
-	return STRENGTH;
-}
 
-// <Миллиньютоны> ---> <Килограмм-силы> //
-double mNewton2kgsNewton(double STRENGTH) {
-	STRENGTH = STRENGTH * 1000 * 0.101972;
-	return STRENGTH;
-}
-
-// <Килограмм-силы> ---> <Миллиньютоны> //
-double kgsNewton2mNewton(double STRENGTH) {
-	STRENGTH = (STRENGTH / 0.101972) * 1000;
-	return STRENGTH;
-}
-
-// <Микроньютоны> ---> <Килограмм-силы> //
-double MkNewton2kgsNewton(double STRENGTH) {
-	STRENGTH = STRENGTH * 1000000 * 0.101972;
-	return STRENGTH;
-}
-
-// <Килограмм-силы> ---> <Микроньютоны> //
-double kgsNewton2MkNewton(double STRENGTH) {
-	STRENGTH = (STRENGTH / 0.101972) * 1000000;
-	return STRENGTH;
-}
-
-// <Килоньютоны> ---> <Килограмм-силы> //
-double kNewton2kgsNewton(double STRENGTH) {
-	STRENGTH = (STRENGTH / 1000) * 0.101972;
-	return STRENGTH;
-}
-
-// <Килограмм-силы> ---> <Килоньютоны> //
-double kgsNewton2kNewton(double STRENGTH) {
-	STRENGTH = (STRENGTH / 0.101972) * 1000;
-	return STRENGTH;
-}
-
-// <Меганьютоны> ---> <Килограмм-силы> //
-double MgNewton2kgsNewton(double STRENGTH) {
-	STRENGTH = STRENGTH / 1000000 * 0.101972;
-	return STRENGTH;
-}
-
-// <Килограмм-силы> ---> <Меганьютоны> //
-double kgsNewton2MgNewton(double STRENGTH) {
-	STRENGTH = (STRENGTH / 0.101972) * 1000000;
-	return STRENGTH;
-}
-
-// <Ньютоны> ---> <Фунт-силы> //
+// <РќСЊСЋС‚РѕРЅ> ---> <Р¤СѓРЅС‚-СЃРёР»Р°> //
 double Newton2fsNewton(double STRENGTH) {
 	STRENGTH = STRENGTH * 0.225;
 	return STRENGTH;
 }
 
-// <Фунт-силы> ---> <Ньютоны> //
+// <РљРёР»РѕРЅСЊСЋС‚РѕРЅ> ---> <РќСЊСЋС‚РѕРЅ> //
+double kNewton2Newton(double STRENGTH) {
+	STRENGTH = STRENGTH * 1000;
+	return STRENGTH;
+}
+
+// <РњРёР»Р»РёРЅСЊСЋС‚РѕРЅ> ---> <РќСЊСЋС‚РѕРЅ> //
+double mNewton2Newton(double STRENGTH) {
+	STRENGTH = STRENGTH / 1000;
+	return STRENGTH;
+}
+
+// <РњРµРіР°РЅСЊСЋС‚РѕРЅ> ---> <РќСЊСЋС‚РѕРЅ> //
+double MgNewton2Newton(double STRENGTH) {
+	STRENGTH = STRENGTH * 1000000;
+	return STRENGTH;
+}
+
+// <РњРёРєСЂРѕРЅСЊСЋС‚РѕРЅ> ---> <РќСЊСЋС‚РѕРЅ> //
+double MkNewton2Newton(double STRENGTH) {
+	STRENGTH = STRENGTH / 1000000;
+	return STRENGTH;
+}
+
+// <РљРёР»РѕРіСЂР°РјРј-СЃРёР»Р°> ---> <РќСЊСЋС‚РѕРЅ> //
+double kgsNewton2Newton(double STRENGTH) {
+	STRENGTH = STRENGTH / 0.101972;
+	return STRENGTH;
+}
+
+// <Р¤СѓРЅС‚-СЃРёР»Р°> ---> <РќСЊСЋС‚РѕРЅ> //
 double fsNewton2Newton(double STRENGTH) {
 	STRENGTH = STRENGTH / 0.225;
-	return STRENGTH;
-}
-
-// <Килоньютоны> ---> <Фунт-силы> //
-double kNewton2fsNewton(double STRENGTH) {
-	STRENGTH = (STRENGTH / 1000) * 0.225;
-	return STRENGTH;
-}
-
-// <Фунт-силы> ---> <Килоньютоны> //
-double fsNewton2kNewton(double STRENGTH) {
-	STRENGTH = (STRENGTH / 0.225) * 1000;
-	return STRENGTH;
-}
-
-// <Меганьютоны> ---> <Фунт-силы> //
-double MgNewton2fsNewton(double STRENGTH) {
-	STRENGTH = (STRENGTH / 1000000) * 0.225;
-	return STRENGTH;
-}
-
-// <Фунт-силы> ---> <Меганьютоны> //
-double fsNewton2MgNewton(double STRENGTH) {
-	STRENGTH = (STRENGTH / 0.225) * 1000000;
-	return STRENGTH;
-}
-
-// <Миллиньютоны> ---> <Фунт-силы> //
-double mNewton2fsNewton(double STRENGTH) {
-	STRENGTH = STRENGTH * 1000 * 0.225;
-	return STRENGTH;
-}
-
-// <Фунт-силы> ---> <Миллиньютоны> //
-double fsNewton2mNewton(double STRENGTH) {
-	STRENGTH = (STRENGTH / 0.225) * 1000;
-	return STRENGTH;
-}
-
-// <Микроньютоны> ---> <Фунт-силы> //
-double MkNewton2fsNewton(double STRENGTH) {
-	STRENGTH = STRENGTH * 1000000 * 0.225;
-	return STRENGTH;
-}
-
-// <Фунт-силы> ---> <Микроньютоны> //
-double fsNewton2MkNewton(double STRENGTH) {
-	STRENGTH = (STRENGTH / 0.225) * 1000000;
-	return STRENGTH;
-}
-
-// <Килограмм-силы> ---> <Фунт-силы> //
-double kgsNewton2fsNewton(double STRENGTH) {
-	STRENGTH = STRENGTH * 2.205;
-	return STRENGTH;
-}
-
-// <Фунт-силы> ---> <Килограмм-силы> //
-double fsNewton2kgsNewton(double STRENGTH) {
-	STRENGTH = STRENGTH / 2.205;
 	return STRENGTH;
 }
