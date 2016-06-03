03b70464080b0deca45d747f108d5692557d9eff
ProkopenkoRoman <long399@mail.ru>
2016-06-03 14:33:53 +0700

    исправленная версия

--- Project/Switches/selectionswitchQuantityOfSubstance.c
+++ Project/Switches/selectionswitchQuantityOfSubstance.c
@@ -14,11 +14,13 @@ double selectionswitchQuantityOfSubstance(double MEASUREMENT)
            "4 - Киломоли;\n"
            "5 - Мегамоли;\n"
            "6 - Фунт-моли;\n\t");
+    scanf("%s", key.in);
     key.input_choice = parsing_id(key.in);
     //scanf("%d", &key.input_choice);
     while(key.input_choice >= 7 || key.input_choice == 0) {
         fprintf(stderr, "\nВы выбрали величину, которой нет в списке доступных величин. \nПожалуйста, сделайте корректный выбор\n\t");
-        scanf("%d", &key.input_choice);
+        scanf("%s", &key.in);
+        key.input_choice = parsing_id(key.in);
     }
 
     printf("Выберите величину, в которую Вы хотите конвертировать Вашу величину.\n"
@@ -29,12 +31,14 @@ double selectionswitchQuantityOfSubstance(double MEASUREMENT)
            "4 - Киломоли;\n"
            "5 - Мегамоли;\n"
            "6 - Фунт-моли;\n\t");
+    scanf("%s", key.out);
     key.output_choice = parsing_id(key.out);
     //scanf("%d", &key.output_choice);
-    while(key.output_choice == key.input_choice || key.input_choice >= 7 || key.input_choice == 0 )  {
+    while(key.output_choice == key.input_choice || key.output_choice >= 7 || key.output_choice == 0 )  {
         fprintf(stderr, "\nВы выбрали величину, которой нет в списке доступных величин, \nили Вы пытаетесь конвертировать "
                 "одну и ту же величину. \nПожалуйста, сделайте корректный выбор\n\t");
-        scanf("%d", &key.output_choice);
+        scanf("%s", &key.out);
+        key.output_choice = parsing_id(key.out);
     }
 
     key.id = key.input_choice * 10 + key.output_choice;
